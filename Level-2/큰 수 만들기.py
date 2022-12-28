49338c3db697e1d472a4581abf0754d57fb8d048
KoKwanwun <dkfajflsla@naver.com>
2022-12-28 17:36:30 +0900

    [Update] Level-2 : 큰 수 만들기 수정 (코드 개선)

--- Level-2/큰 수 만들기.py
+++ Level-2/큰 수 만들기.py
@@ -2,15 +2,10 @@ def solution(number, k):
     stack = []
 
     for num in number:
-        if(len(number) == 0):
-            stack.append(num)
-            continue
-        if(k > 0 and stack[-1] < num):
+        while k > 0 and stack and stack[-1] < num:
             stack.pop()
             k -= 1
-            if(k <= 0):
-                break
-        stack.append(num)
+        stack.append(num)   # k는 number의 자릿수 미만이기 때문에 최소 stack의 자릿수는 1
 
     for _ in range(k):
         stack.pop()
