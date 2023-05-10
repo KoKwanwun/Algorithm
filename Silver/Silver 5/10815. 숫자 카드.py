1bf19087f09f23ad34756d981e9282b044814371
KoKwanwun <dkfajflsla@naver.com>
2023-05-10 11:33:11 +0900

    [Update] S5 : 숫자 카드 (주석 추가)

--- Silver/Silver 5/10815. 숫자 카드.py
+++ Silver/Silver 5/10815. 숫자 카드.py
@@ -1,6 +1,8 @@
+# 카드 숫자의 가장 크고 작은 수의 절댓값인 10000000의 +1개만큼의 리스트 생성
 mlist = [0] * 10000001
 result = []
 
+# 상근이가 가지고 있는 숫자 카드가 양수일 때는 해당 인덱스의 +1, 음수일 때는 해당 인덱스의 +2
 n = int(input())
 for num in list(map(int, input().split())):
     if num >= 0:
@@ -8,6 +10,9 @@ for num in list(map(int, input().split())):
     else:
         mlist[abs(num)] += 2
 
+# 1인 경우 : 양수만
+# 2인 경우 : 음수만
+# 3인 경우 : 양수 음수 둘다
 m = int(input())
 for num in list(map(int, input().split())):
     if num >= 0:
