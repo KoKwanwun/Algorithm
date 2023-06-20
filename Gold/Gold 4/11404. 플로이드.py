a70f9aade507e8d78426d97a73989b38795631f6
KoKwanwun <dkfajflsla@naver.com>
2023-06-20 10:13:17 +0900

    [Update] G4 : 플로이드 (비용 값 변경으로 통과)

--- Gold/Gold 4/11404. 플로이드.py
+++ Gold/Gold 4/11404. 플로이드.py
@@ -1,10 +1,11 @@
 # 플로이드 와샬 알고리즘
 import sys
 input = sys.stdin.readline
+inf = 1000000000
 
 n = int(input())
 
-graph = [[100001] * (n+1) for _ in range(n+1)]
+graph = [[inf] * (n+1) for _ in range(n+1)]
 
 for _ in range(int(input())):
     a, b, c = map(int, input().split())
@@ -21,7 +22,7 @@ for k in range(1, n+1):
 
 for i in range(1, n+1):
     for j in range(1, n+1):
-        if graph[i][j] == 100001:
+        if graph[i][j] == inf:
             print(0, end=" ")
         else:
             print(graph[i][j], end=" ")
