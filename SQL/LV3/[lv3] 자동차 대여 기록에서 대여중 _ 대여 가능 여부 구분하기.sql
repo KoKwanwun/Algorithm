3993ab8f2ae800e2e2c17e810b9cf112dd868cd7
KoKwanwun <dkfajflsla@naver.com>
2023-06-30 23:51:48 +0900

    [Add] SQL lv3 : 자동차 대여 기록에서 대여중 / 대여 가능 여부 구분하기

---
+++ SQL/LV3/[lv3] 자동차 대여 기록에서 대여중 _ 대여 가능 여부 구분하기.sql
@@ -0,0 +1,7 @@
+-- 2022년 10월 16일에 대여 중인 자동차인 경우 '대여중' 이라고 표시하고, 대여 중이지 않은 자동차인 경우 '대여 가능'을 표시하는 컬럼(컬럼명: AVAILABILITY)을 추가하여 자동차 ID와 AVAILABILITY 리스트를 출력
+-- 반납 날짜가 2022년 10월 16일인 경우에도 '대여중'으로 표시
+-- 자동차 ID를 기준으로 내림차순 정렬
+SELECT DISTINCT CAR_ID, 
+IF(CAR_ID IN (SELECT DISTINCT CAR_ID FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY WHERE '2022-10-16' BETWEEN START_DATE AND END_DATE), '대여중', '대여 가능') AS AVAILABILTIY 
+FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY 
+ORDER BY CAR_ID DESC
