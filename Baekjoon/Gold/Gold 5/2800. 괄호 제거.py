e0ffaa2f27754ec5aed1f62e228d7c0e6f841863
KoKwanwun <dkfajflsla@naver.com>
2023-08-08 08:04:56 +0900

    [Baekjoon] G5 : 괄호 제거 (주석 추가)

--- Baekjoon/Gold/Gold 5/2800. 괄호 제거.py
+++ Baekjoon/Gold/Gold 5/2800. 괄호 제거.py
@@ -13,11 +13,13 @@ for idx, word in enumerate(mlist):
         bracketStack.append((stack.pop(), idx))
         
 for i in range(1, len(bracketStack) + 1):
+    # 조합으로 제거할 괄호쌍 뽑기
     comb = combinations(bracketStack, i)
     
     for j in comb:
+        # 문자열 복사
         tmp = mlist[:]
-        print(tmp)
+
         for k in j:
             tmp[k[0]] = ""
             tmp[k[1]] = ""
